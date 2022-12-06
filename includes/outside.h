3ab605da7cf578004d48eaf9c9ba8500716e1435
andreyvdl <andreyvictordantasleal2016@gmail.com>
2022-12-05 21:48:38 -0300

    include: added outside functions include

---
+++ includes/outside.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   outside.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 23:32:44 by adantas-          #+#    #+#             */
+/*   Updated: 2022/12/01 23:32:44 by adantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OUTSIDE_H
+# define OUTSIDE_H
+# include <stdarg.h>
+# include <stdlib.h>
+
+long long int	sum(long long unsigned int n, ...);
+
+#endif
