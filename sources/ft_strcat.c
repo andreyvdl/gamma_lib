0db266a3425ceec6d03a3ac78057dd4cebc470d0
adantas- <adantas-@student.42sp.org.br>
2022-12-07 14:29:16 -0300

    add: new functions
    functions added:
    ft_islower
    ft_isupper
    ft_strcapitalize
    ft_strcat
    ft_strcmp
    ft_strlowcase
    ft_strupcase

---
+++ sources/ft_strcat.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/07 14:08:22 by adantas-          #+#    #+#             */
+/*   Updated: 2022/12/07 14:15:58 by adantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+char	*ft_strcat(char *dest, char *src)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j])
+		dest[i++] = src[j++];
+	dest[i] = 0;
+	return (dest);
+}
