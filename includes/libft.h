ee78f4c314e5d06cab0e7728fa7c6a3b7169fb25
adantas- <adantas-@student.42sp.org.br>
2022-12-07 14:27:36 -0300

    mod: added more functions

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 15:00:37 by adantas-          #+#    #+#             */
-/*   Updated: 2022/12/06 22:52:49 by adantas-         ###   ########.fr       */
+/*   Updated: 2022/12/07 14:23:35 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,49 +21,56 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-// check things and returns true or false
+// check things & returns true or false
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
-int		ft_isdigit(int c);
 int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_islower(int c);
 int		ft_isprint(int c);
+int		ft_isupper(int c);
 
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 
 // work with memory
 void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 
 // work with strings
-char	*ft_itoa(int n);
-size_t	ft_strlen(const char *s);
 int		ft_atoi(const char *nptr);
-char	*ft_strdup(const char *s);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strcpy(char *dst, char *src);
+char	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
-char	*ft_strrchr(const char *s, int c);
+char	*ft_strcapitalize(char *str);
+char	*ft_strcat(char *dest, char *src);
+char	*ft_strchr(const char *s, int c);
+int		ft_strcmp(char *s1, char *s2);
+char	*ft_strcpy(char *dst, char *src);
+char	*ft_strdup(const char *s);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+char	*ft_strlowcase(char *str);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strupcase(char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
-// print file descriptors
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *s, int fd);
+// print & file descriptors
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
@@ -78,7 +85,6 @@ void	ft_lstiter(t_list *lst, void (*f)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-
 
 // work with pointers
 void	ft_swap(void *a, void *b);
