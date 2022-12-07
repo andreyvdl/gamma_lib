4e06ca936a4db3e7ffe7bcda50b7ce6c8410db75
adantas- <adantas-@student.42sp.org.br>
2022-12-07 15:35:09 -0300

    fix: fixed variable names

--- sources/ft_strstr.c
+++ sources/ft_strstr.c
@@ -6,29 +6,29 @@
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 15:23:36 by adantas-          #+#    #+#             */
-/*   Updated: 2022/12/07 15:30:32 by adantas-         ###   ########.fr       */
+/*   Updated: 2022/12/07 15:34:41 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-char	*ft_strstr(const char *haystack, const char *needle)
+char	*ft_strstr(const char *str, const char *to_find)
 {
 	size_t	i;
 	size_t	j;
 
-	if (!needle[0])
-		return ((char *)haystack);
-	else if (!needle[0] && !haystack[0])
-		return ((char *)haystack);
+	if (!to_find[0])
+		return ((char *)str);
+	else if (!to_find[0] && !str[0])
+		return ((char *)str);
 	i = -1;
-	while (haystack[++i])
+	while (str[++i])
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j])
+		while (str[i + j] == to_find[j])
 		{
-			if (needle[j + 1] == 0)
-				return (&((char *)haystack)[i]);
+			if (to_find[j + 1] == 0)
+				return (&((char *)str)[i]);
 			j++;
 		}
 	}
