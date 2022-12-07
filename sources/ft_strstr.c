aca8496916d6b6cbb965277687197504be8e088b
adantas- <adantas-@student.42sp.org.br>
2022-12-07 15:32:55 -0300

    add: ft_strstr.c

---
+++ sources/ft_strstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/07 15:23:36 by adantas-          #+#    #+#             */
+/*   Updated: 2022/12/07 15:30:32 by adantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!needle[0])
+		return ((char *)haystack);
+	else if (!needle[0] && !haystack[0])
+		return ((char *)haystack);
+	i = -1;
+	while (haystack[++i])
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j])
+		{
+			if (needle[j + 1] == 0)
+				return (&((char *)haystack)[i]);
+			j++;
+		}
+	}
+	return (0x0);
+}
