c457c902da0a9a199b4a2fc2015d75f7aeaad5be
andreyvdl <andreyvictordantasleal2016@gmail.com>
2022-12-08 00:03:55 -0300

    2tired

--- sources/ft_sqrt.c
+++ sources/ft_sqrt.c
@@ -1,22 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_cubed.c                                         :+:      :+:    :+:   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/07 22:02:35 by adantas-          #+#    #+#             */
-/*   Updated: 2022/12/07 22:46:30 by adantas-         ###   ########.fr       */
+/*   Created: 2022/12/07 22:54:36 by adantas-          #+#    #+#             */
+/*   Updated: 2022/12/08 00:02:36 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-long long int	ft_cubed(long long int n)
+long double	ft_sqrt(long double n)
 {
-	if (n == 0)
+	long double	total;
+	size_t		fat;
+
+	fat = 2;
+	total = 1;
+	if (n <= 0 || n == 0x0)
 		return (0);
-	if (n == 1)
-		return (1);
-	return (n * n * n);
+	while (n != 1)
+	{
+		if (n <= fat)
+			break ;
+		else if (n % fat == 0)
+		{
+			total *= fat;
+			n /= fat;
+		}
+		else
+		{
+			++fat;
+			while (fat % 2 != 0 || fat % 3 != 0 || fat % 5 != 0 || fat % 7 != 0)
+				++fat;
+		}
+	}
+	return (total * n);
 }
