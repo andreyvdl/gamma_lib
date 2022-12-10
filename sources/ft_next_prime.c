f82354e114527ca1ba537172e49bc66d543a8c46
andreyvdl <andreyvictordantasleal2016@gmail.com>
2022-12-10 00:11:26 -0300

    add: added 2 functions
    ft_isprime
    ft_next_prime

---
+++ sources/ft_next_prime.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_next_prime.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 22:12:47 by adantas-          #+#    #+#             */
+/*   Updated: 2022/12/09 22:39:53 by adantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+long long int	ft_next_prime(long long int nb)
+{
+	if (nb <= 1)
+		return (2);
+	else if (nb == 2)
+		return (3);
+	else if (nb == 3)
+		return (5);
+	else if (nb == 5)
+		return (7);
+	else if (nb == 7)
+		return (11);
+	while (nb % 2 == 0 || nb % 3 == 0 || nb % 5 == 0 || nb % 7 == 0)
+	{
+		if (nb < 0)
+			return (-1);
+		++nb;
+	}
+	return (nb);
+}
