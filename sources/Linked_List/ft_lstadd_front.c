dc244399c682afcc44d0cf309e173b9e38392a37
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-04-18 11:18:50 -0300

    R+:Main linked list function remade, now the rest

--- sources/Linked_List/ft_lstadd_front.c
+++ sources/Linked_List/ft_lstadd_front.c
@@ -1,47 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/19 17:16:43 by adantas-          #+#    #+#             */
-/*   Updated: 2023/01/31 11:25:28 by adantas-         ###   ########.fr       */
+/*   Created: 2023/04/18 09:55:00 by adantas-          #+#    #+#             */
+/*   Updated: 2023/04/18 10:15:20 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
-#include "../includes/libft_structs.h"
+#include "../../includes/libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list	*last;
-
-	if (new == 0x0)
-		return ;
-	else if (*lst == 0x0)
-		*lst = new;
-	else
-	{
-		last = ft_lstlast(*lst);
-		last->next = new;
-	}
-}
-
+/**
+ * @brief Add a new node at the beginning of the list; If the list is NULL, the
+ * new node will be the first node; If the new node is NULL, nothing will
+ * happen;
+ * 
+ * @param lst The address of a pointer to the first link of a list;
+ * @param new The pointer to the new node to be added to the list;
+ */
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (new == 0x0)
+	if (new == NULL)
 		return ;
 	new->next = *lst;
 	*lst = new;
-}
-
-void	ft_lstmerge(t_list **begin_list1, t_list *begin_list2)
-{
-	t_list	*current;
-
-	if (!begin_list1 || !begin_list2)
-		return ;
-	current = ft_lstlast(*begin_list1);
-	current->next = begin_list2;
 }
--- sources/Linked_List/ft_lstdelone.c
+++ sources/Linked_List/ft_lstdelone.c
@@ -1,34 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelete.c                                     :+:      :+:    :+:   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/19 20:39:15 by adantas-          #+#    #+#             */
-/*   Updated: 2023/01/31 11:26:01 by adantas-         ###   ########.fr       */
+/*   Created: 2023/04/18 11:13:49 by adantas-          #+#    #+#             */
+/*   Updated: 2023/04/18 11:17:13 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
-#include "../includes/libft_structs.h"
+#include "../../includes/libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	t_list	*free_this;
-
-	while (*lst != 0x0)
-	{
-		free_this = *lst;
-		*lst = (*lst)->next;
-		del(free_this->content);
-		free(free_this);
-	}
-	lst = 0x0;
-}
-
+/**
+ * @brief Deletes and frees a node from a list; If the node is NULL nothing will
+ * happen;
+ * 
+ * @param lst A pointer to the node to be deleted;
+ * @param del A pointer to the function that will delete the content of the node;
+ */
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
+	if (lst == NULL)
+		return ;
 	del(lst->content);
 	free(lst);
 }
